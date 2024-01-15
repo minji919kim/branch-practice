0b4ef79ff97063dc745d288da7b195272a80d51a
minji919kim <minji919kim@gmail.com>
2024-01-15 00:11:26 -0500

    feat: Changed to Fizz function

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-from random import randint
-from random import choice
+def fizz()->None:
+    for i in range(1,16):
+        if i%3==0:
+            print("fizz")
+        else:
+            print(i)
 
-def get_randint() -> list:
-    return [randint(1,50) for _ in range(7)]
-
-def get_choice() -> list:
-    return [choice(range(1, 50+1)) for _ in range(7)]
-
-fruits = [
-        'apple', 
-        'banana', 
-        'kiwi'
-]
-
-foods = [
-        'pasta', 
-        'pizza',
-        'stew',
-]
-
-if __name__=='__main__':
-    print(get_randint())
-    print(get_choice())
+if __name__=="__main__":
+    fizz()
 
