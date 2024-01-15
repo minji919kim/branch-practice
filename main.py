73d52d4644b3e694f4ef0ba17f668f256afe693e
minji919kim <minji919kim@gmail.com>
2024-01-14 22:10:57 -0500

    Trailing Comma Result

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ def get_randint() -> list:
 def get_choice() -> list:
     return [choice(range(1, 45+1)) for _ in range(7)]
 
+fruits = [
+        'apple', 
+        'banana', 
+        'pineapple'
+]
+
+foods = [
+        'pasta', 
+        'pizza',
+        'stew',
+]
+
+
 if __name__=='__main__':
     print(get_randint())
     print(get_choice())
